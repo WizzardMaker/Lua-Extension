92286aef362963b970c1e8934848ab8bab42f3d5
WizzardMaker <wizzardmaking@hotmail.com>
2015-02-10 19:07:38 +0100

    Typo!
    Just fixed a typo in the include.h

--- enigma-dev/ENGIMAsystem/SHELL/Universal_System/Extensions/Lua/include.h
+++ enigma-dev/ENGIMAsystem/SHELL/Universal_System/Extensions/Lua/include.h
@@ -9,6 +9,6 @@ namespace enigma_user{
 //lua_State lua_start(void);
 //lua_State lua_stop(lua_State *Lua);
 variant lua_execute_file(const char* file);
-void lua_execute_string(const char* String);
+variant lua_execute_string(const char* String);
 //variant lua_call_function(lua_State *Lua,string file, string function, int results,variant argument0 , ...);
 }
