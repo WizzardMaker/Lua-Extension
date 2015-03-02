7e997845992b81631102d53724ba4e07eb28efdc
WizzardMaker <wizzardmaking@hotmail.com>
2015-03-02 15:16:06 +0100

    Branch to remove!

---
+++ enigma-dev/ENIGMAsystem/SHELL/Universal_System/Extensions/Lua/include.h
@@ -0,0 +1,18 @@
+#include "../../var4.h"
+#include "../../dynamic_args.h"
+#include "../../object.h"
+#include "../../instance_iterator.h"
+#include "../../scalar.h"
+#include "Widget_Systems/widgets_mandatory.h"
+#include <stdarg.h>
+#include <array>
+#include <functional>
+#include <LuaState.h>
+namespace enigma_user{
+void lua_start(string filepath);
+void lua_stop();
+void lua_add_var(string name, int var);
+void lua_add_var(string name, string var);
+void lua_execute_file();
+
+}
---
+++ test1/instance_create.h
@@ -0,0 +1,3 @@
+namespace enigma_user{
+	void instance_create(gs_scalar x, gs_scalar y, int id);
+}
