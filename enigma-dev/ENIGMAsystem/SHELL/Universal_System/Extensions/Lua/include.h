23c5a6a66d12062636d93524e207c5c24eeb7ef9
WizzardMaker <wizzardmaking@hotmail.com>
2015-03-02 15:42:09 +0100

    Finished Lua
    added icon

--- enigma-dev/ENIGMAsystem/SHELL/Universal_System/Extensions/Lua/include.h
+++ enigma-dev/ENIGMAsystem/SHELL/Universal_System/Extensions/Lua/include.h
@@ -1,17 +1,17 @@
 #include "../../var4.h"
 #include "../../dynamic_args.h"
+#include "../../object.h"
+#include "../../instance_iterator.h"
+#include "../../scalar.h"
+#include "Widget_Systems/widgets_mandatory.h"
 #include <stdarg.h>
 #include <array>
 #include <functional>
-#include <luastate.h>
-
+#include <LuaState.h>
 namespace enigma_user{
-variant lua_execute_file(string file);
-variant lua_execute_string(string String);
-variant lua_get_variable(string file, string variable);
-variant luaF_execute_file_int1(string filepath,const std::function<int(int)> &f);
-variant luaF_execute_file_int2(string filepath,const std::function<int(int, int)> &f,int arguments);
-variant luaF_execute_file_int3(string filepath,const std::function<int(int, int, int)> &f,int arguments);
-variant luaF_execute_file_str1(string filepath,const std::function<int(string)> &f,int arguments);
-variant luaF_execute_file_str2(string filepath,const std::function<int(string, string)> &f,int arguments);
+void lua_start(string filepath);
+void lua_stop();
+void lua_add_var(string name, int var);
+void lua_add_var(string name, string var);
+void lua_execute_file();
 }
